afe51cf03e0a8f6d1e055ab1656e3ad711e18059
Levin Gsell <levin.gsell@bluewin.ch>
2023-08-21 17:19:54 +0200

    Added script

---
+++ script.js
@@ -0,0 +1,45 @@
+const fs = require('fs')
+const path = require('path')
+
+const downloadPath = path.resolve('/Users/thesolian/Downloads')
+const bbbPath = path.resolve(
+  '/Users/thesolian/Library/Mobile Documents/com~apple~CloudDocs/Schule/BBB'
+)
+
+const files = fs.readdirSync(downloadPath)
+
+const bbbFiles = files.filter((file) => {
+  file.startsWith('LA_') || file.startsWith('PR_')
+})
+
+if (bbbFiles.length === 0) console.log('No files found')
+
+bbbFiles.forEach((file) => {
+  if (file.startsWith('LA_')) {
+    const fileWithoutEnding = file.slice(0, -'.docx'.length)
+    const filePath = path.resolve(downloadPath, file)
+
+    const module = file.substring(3, 6)
+    if (module === 'ILA') return
+    const modulePath = path.resolve(bbbPath, module)
+
+    if (!fs.existsSync(modulePath)) fs.mkdirSync(modulePath)
+    if (fs.existsSync(path.resolve(modulePath, fileWithoutEnding))) return
+
+    fs.mkdirSync(path.resolve(modulePath, fileWithoutEnding))
+
+    fs.renameSync(filePath, path.resolve(modulePath, fileWithoutEnding, file))
+  } else if (file.startsWith('PR_')) {
+    const filePath = path.resolve(downloadPath, file)
+
+    const module = file.substring(3, 6)
+    if (module === 'ILA') return
+    const modulePath = path.resolve(bbbPath, module)
+
+    if (!fs.existsSync(path.resolve(modulePath, 'Theorie')))
+      fs.mkdirSync(path.resolve(modulePath, 'Theorie'))
+    if (fs.existsSync(path.resolve(modulePath, 'Theorie', file))) return
+
+    fs.renameSync(filePath, path.resolve(modulePath, 'Theorie', file))
+  }
+})
